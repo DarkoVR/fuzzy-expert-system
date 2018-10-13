6bc1c0ae3399a7266a0c2a58a580f4a6aee51e89
Marco Vazquez <marcovazquez77@hotmail.com>
2018-10-13 17:38:52 -0500

    Constants class added to put immutable data

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,9 @@
+import java.io.File
+
+object Constants {
+    val STRING_LENGTH = 32
+    val COORDINATES_LENGTH = 8
+    val LABELS_LENGTH = 8
+    val LV_REGISTER_SIZE = 1600
+    val file = File("linguistic_variable_master")
+}
