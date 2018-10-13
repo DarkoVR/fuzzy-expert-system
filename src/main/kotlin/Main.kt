ba661df80b7633fede6497378316648a822ac44c
Marco Vazquez <marcovazquez77@hotmail.com>
2018-10-13 17:40:07 -0500

    Main file test added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,40 @@
+import files.LVFileHandler
+import models.Coordinate
+import models.LVRegister
+import models.Label
+
 fun main(args: Array<String>) {
-    println("Hello world!")
+    val data: ArrayList<LVRegister> = ArrayList()
+    data.add(LVRegister("trabajo_en_equipo", arrayListOf(
+            Label("malo",
+                    arrayListOf(
+                            Coordinate(2.0,4.0),
+                            Coordinate(3.0,6.0),
+                            Coordinate(26.0,45.0),
+                            Coordinate(62.0,84.0)
+                    )
+            ),
+            Label("regular",
+                    arrayListOf(
+                            Coordinate(2.0,4.0),
+                            Coordinate(3.0,6.0),
+                            Coordinate(26.0,45.0),
+                            Coordinate(62.0,84.0)
+                    )
+            ),
+            Label("bueno",
+                    arrayListOf(
+                            Coordinate(2.0,4.0),
+                            Coordinate(3.0,6.0),
+                            Coordinate(26.0,45.0),
+                            Coordinate(62.0,84.0)
+                    )
+            )
+    ), 0))
+
+    val fileHandler = LVFileHandler()
+    data.forEach {
+        fileHandler.write(it)
+    }
+    fileHandler.read(0)
 }
